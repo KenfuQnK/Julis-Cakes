aee5d3d0bdfe67a0f05579149069681a16cc3034
KenfuQnK <86153113+KenfuQnK@users.noreply.github.com>
2025-04-21 20:28:45 +0200

    Create main.js

---
+++ rendered/main.js
@@ -0,0 +1,6 @@
+// Archivo principal de JavaScript para Julis Cakes
+
+// Aquí puedes agregar tus scripts para manejar la interactividad de tu sitio web.
+// Por ejemplo, añadir un mensaje de bienvenida en la consola:
+
+console.log("Bienvenido a Julis Cakes!");
